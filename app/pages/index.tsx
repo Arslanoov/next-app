78cdbffe7eedfca72a6df572f786e79cd33ba81a
Arslanoov <rasularslanoov@gmail.com>
2021-03-31 20:15:11 +0500

    Added sass

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,10 +2,13 @@ import * as React from "react"
 
 import MainLayout from "../layouts/main"
 
+import classes from "../styles/Home.module.sass"
+
 export default function Home() {
   return (
     <MainLayout title={'Home'}>
       <div>Home page</div>
+      <div className={classes.example}>Some text</div>
     </MainLayout>
   )
 }
