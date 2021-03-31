b49f3db7800ba75041cbad24b3c9118b14a03ad6
Arslanoov <rasularslanoov@gmail.com>
2021-03-31 19:36:45 +0500

    Added typescript

---
+++ app/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import MainLayout from "../layouts/main"
+
+export default function Home() {
+  return (
+    <MainLayout title={'Home'}>
+      <div>Home page</div>
+    </MainLayout>
+  )
+}
