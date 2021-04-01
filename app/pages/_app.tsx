a68b122687ee491824025f2a5f8df18ca55c1e5d
Arslanoov <rasularslanoov@gmail.com>
2021-04-01 11:05:48 +0500

    Added normalize

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import { AppProps } from "next/app"
 
-import "../styles/globals.sass"
+import "../styles/main.sass"
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -7,7 +7,7 @@ import classes from "../styles/Home.module.sass"
 const Home: React.FC = () => {
   return (
     <MainLayout title="Home">
-      <div>Home page</div>
+      <h1>Home page</h1>
       <div className={classes.example}>Some text</div>
     </MainLayout>
   )
